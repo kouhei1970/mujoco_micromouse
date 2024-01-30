bfa6b6d18aa42b5570c4c710ff96d87fdf4da50a
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2024-01-30 12:45:58 +0900

    update

--- mujoco_micromouse.py
+++ mujoco_micromouse.py
@@ -26,6 +26,11 @@ def get_gyro(model, data):
   gz = data.sensor('Gyro').data[2]
   return gx,gy,gz
 
+paused = False
+def key_callback(keycode):
+  if chr(keycode) == ' ':
+    global paused
+    paused = not paused
 
 #Create model
 model = mujoco.MjModel.from_xml_path('mouse_in_maze.xml')
@@ -66,47 +71,47 @@ wheel_r = 0.0135
 now = 0.0
 past = 0.0
 turn_flag = 0
-with mujoco.viewer.launch_passive(model, data) as viewer:
+with mujoco.viewer.launch_passive(model, data, key_callback=key_callback) as viewer:
   while viewer.is_running():
+    if not paused:
+      lf, ls, rs, rf = get_distance(model, data)
+      ax, ay, az = get_accel(model, data)
+      gx, gy, gz = get_gyro(model, data)
 
-    lf, ls, rs, rf = get_distance(model, data)
-    ax, ay, az = get_accel(model, data)
-    gx, gy, gz = get_gyro(model, data)
+      #Control
+      err = ls - rs
+      velocity = 0.05
+      k= 0.6
+      if turn_flag==1 or (lf<0.05 and rf <0.05):
+        turn_flag = 1
+        right_mot =  0.06
+        left_mot  = -0.06
+        if lf > 0.09 and rf > 0.09:
+          turn_flag = 0
+        #print('Turn')
+      else:
+        right_mot = velocity + k * err
+        left_mot =  velocity - k * err
+        #print('Foward')
 
-    #Control
-    err = ls - rs
-    velocity = 0.05
-    k= 0.6
-    if turn_flag==1 or (lf<0.05 and rf <0.05):
-      turn_flag = 1
-      right_mot =  0.06
-      left_mot  = -0.06
-      if lf > 0.09 and rf > 0.09:
-         turn_flag = 0
-      #print('Turn')
-    else:
-      right_mot = velocity + k * err
-      left_mot =  velocity - k * err
-      #print('Foward')
+      #Move
+      action(model, data, left_mot, right_mot)
 
-    #Move
-    action(model, data, left_mot, right_mot)
-
-    #Simulation
-    mujoco.mj_step(model, data)
-    
-    #Vizualize
-    now = data.time
-    if now-past>0.01:
-      past = now
-      viewer.sync()
-      #Sensor Data Show
-      #print(now, ax, ay, az, gx, gy, gz)
-      #print(lf,ls,rs,rf)
-      #print(data.sensordata)
-      #print(data.sensor('Gyro').data[0])
-      print(now,\
-             data.actuator('right').length[0],   data.actuator('left').length[0],\
-             data.actuator('right').velocity[0], data.actuator('left').velocity[0],\
-             ax, ay, az, gx, gy, gz)
+      #Simulation
+      mujoco.mj_step(model, data)
+      
+      #Vizualize
+      now = data.time
+      if now-past>0.01:
+        past = now
+        viewer.sync()
+        #Sensor Data Show
+        #print(now, ax, ay, az, gx, gy, gz)
+        #print(lf,ls,rs,rf)
+        #print(data.sensordata)
+        #print(data.sensor('Gyro').data[0])
+        print(now,\
+              data.actuator('right').length[0],   data.actuator('left').length[0],\
+              data.actuator('right').velocity[0], data.actuator('left').velocity[0],\
+              ax, ay, az, gx, gy, gz)
       
